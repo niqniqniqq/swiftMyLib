78ad33c9632c65b2e38320e7a691906371cc1d4f
usr0301057 <usr0301057@APGA0013.local>
2016-10-05 12:30:20 +0900

    add CostumLabel.swift

---
+++ CostumLabel.swift
@@ -0,0 +1,24 @@
+class CostumLabel: UILabel {
+    
+    var padding = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    
+    //padding設定 
+    func setPadding(top : CGFloat, left: CGFloat, bottom: CGFloat, right: CGFloat){
+        padding.top = top
+        padding.left = left
+        padding.right = right
+        padding.bottom = bottom
+    }
+    
+    override func drawTextInRect(rect: CGRect) {
+        let newRect = UIEdgeInsetsInsetRect(rect, padding)
+        super.drawTextInRect(newRect)
+    }
+    
+    override func intrinsicContentSize() -> CGSize {
+        var intrinsicContentSize = super.intrinsicContentSize()
+        intrinsicContentSize.height += padding.top + padding.bottom
+        intrinsicContentSize.width += padding.left + padding.right
+        return intrinsicContentSize
+    }
+}
